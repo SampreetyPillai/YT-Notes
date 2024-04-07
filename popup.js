aa0b23e3b2ccbd751841793a106d4b5fadfcf13a
shanmukha priya <shanmukha.m.priya@gmail.com>
2024-04-07 15:54:00 +0530

    some changesss

--- popup.js
+++ popup.js
@@ -1,17 +1,7 @@
-import firebase from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB7xNZ46ljOSgfMYjOfd1ZcdafPkTscjL4",
-  authDomain: "ytyt-ae2fd.firebaseapp.com",
-  projectId: "ytyt-ae2fd",
-  storageBucket: "ytyt-ae2fd.appspot.com",
-  messagingSenderId: "625413981572",
-  appId: "1:625413981572:web:d71a68a1ae47a14fc909d1",
-  measurementId: "G-KJ6JEZ4J35"
-};
-const app = initializeApp(firebaseConfig);
+
 document.addEventListener('DOMContentLoaded', documentEvents  , false);
-console.log(app);
+
 function myAction(input) { 
     console.log("input value is : " + input.value);
     alert("The entered data is : " + input.value);
@@ -23,8 +13,17 @@ function myAction(input) {
 
 function documentEvents() {    
   document.getElementById('ok_btn').addEventListener('click', 
-    function() { myAction(document.getElementById('name_textbox'));
+    function() { var a=document.getElementById('name_textbox'); var ti= document.getElementById('title'); var text=ti.value+"\n----------------\n"+a.value;downloadTextFile(text);
   });
 
+  function downloadTextFile(content) {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    chrome.downloads.download({
+        url: url,
+        filename: 'text_content.txt',
+        saveAs: true
+    });
+}
   // you can add listeners for other objects ( like other buttons ) here 
 }
