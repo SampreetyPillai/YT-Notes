1d80ab12cedd518089e1c0f23cbb1effc4797426
shanmukha priya <shanmukha.m.priya@gmail.com>
2024-04-07 15:31:53 +0530

    some changes

--- popup.js
+++ popup.js
@@ -1,8 +1,21 @@
-document.addEventListener('DOMContentLoaded', documentEvents  , false);
+import firebase from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB7xNZ46ljOSgfMYjOfd1ZcdafPkTscjL4",
+  authDomain: "ytyt-ae2fd.firebaseapp.com",
+  projectId: "ytyt-ae2fd",
+  storageBucket: "ytyt-ae2fd.appspot.com",
+  messagingSenderId: "625413981572",
+  appId: "1:625413981572:web:d71a68a1ae47a14fc909d1",
+  measurementId: "G-KJ6JEZ4J35"
+};
+const app = initializeApp(firebaseConfig);
+document.addEventListener('DOMContentLoaded', documentEvents  , false);
+console.log(app);
 function myAction(input) { 
     console.log("input value is : " + input.value);
     alert("The entered data is : " + input.value);
+
     // do processing with data
     // you need to right click the extension icon and choose "inspect popup"
     // to view the messages appearing on the console.
